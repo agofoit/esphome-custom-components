c5b0c52b80db8f5f39825cbc187c0e29a0567875
Anne <anne.gofoit@gmail.com>
2024-06-20 15:45:36 +0200

    Do not use neopixelbus and just make it easier

--- custom_components/cloud_light/__init__.py
+++ custom_components/cloud_light/__init__.py
@@ -2,10 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components.light.types import AddressableLightEffect
 from esphome.components.light.effects import register_addressable_effect
-from esphome.const import CONF_ID, CONF_NAME, CONF_CHANNELS
-
-AUTO_LOAD = ["socket"]
-DEPENDENCIES = ["network"]
+from esphome.const import CONF_ID, CONF_NAME
 
 cloud_light_ns = cg.esphome_ns.namespace("cloud_light")
 YellowAddressableLightEffect = cloud_light_ns.class_(
@@ -16,12 +13,6 @@ CyanAddressableLightEffect = cloud_light_ns.class_(
 )
 CloudLightComponent = cloud_light_ns.class_("CloudLightComponent", cg.Component)
 
-CHANNELS = {
-    "MONO": cloud_light_ns.CloudLight_MONO,
-    "RGB": cloud_light_ns.CloudLight_RGB,
-    "RGBW": cloud_light_ns.CloudLight_RGBW,
-}
-
 CONF_Yellow_ID = "yellow_id"
 CONF_Cyan_ID = "cyan_id"
 
@@ -30,7 +21,6 @@ CONFIG_SCHEMA = cv.Schema(
         cv.GenerateID(): cv.declare_id(CloudLightComponent),
     }
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -42,15 +32,12 @@ async def to_code(config):
     YellowAddressableLightEffect,
     "Yellow",
     {
-        cv.GenerateID(CONF_Yellow_ID): cv.use_id(CloudLightComponent),  # Reuse the existing component
-        cv.Optional(CONF_CHANNELS, default="RGB"): cv.one_of(*CHANNELS, upper=True),
+        cv.GenerateID(CONF_Yellow_ID): cv.use_id(CloudLightComponent),
     },
 )
 async def yellow_light_effect_to_code(config, effect_id):
     parent = await cg.get_variable(config[CONF_Yellow_ID])
-
     effect = cg.new_Pvariable(effect_id, config[CONF_NAME])
-    cg.add(effect.set_yellow(parent))  # Make sure this method exists in your effect class
     return effect
 
 @register_addressable_effect(
@@ -58,13 +45,10 @@ async def yellow_light_effect_to_code(config, effect_id):
     CyanAddressableLightEffect,
     "Cyan",
     {
-        cv.GenerateID(CONF_Cyan_ID): cv.use_id(CloudLightComponent),  # Reuse the existing component
-        cv.Optional(CONF_CHANNELS, default="RGB"): cv.one_of(*CHANNELS, upper=True),
+        cv.GenerateID(CONF_Cyan_ID): cv.use_id(CloudLightComponent),
     },
 )
 async def cyan_light_effect_to_code(config, effect_id):
     parent = await cg.get_variable(config[CONF_Cyan_ID])
-
     effect = cg.new_Pvariable(effect_id, config[CONF_NAME])
-    cg.add(effect.set_cyan(parent))  # Make sure this method exists in your effect class
     return effect
--- custom_components/cloud_light/cloud_light.h
+++ custom_components/cloud_light/cloud_light.h
@@ -1,47 +1,115 @@
 #pragma once
 
-#include "esphome/components/socket/socket.h"
 #include "esphome/core/component.h"
-
-#include <cinttypes>
-#include <map>
-#include <memory>
-#include <set>
-#include <vector>
-
-#include <NeoPixelBus.h>
+#include "esphome/components/light/addressable_light_effect.h"
 
 namespace esphome {
 namespace cloud_light {
 
-class YellowAddressableLightEffect;
-class CyanAddressableLightEffect;
+class CloudLightComponent : public esphome::Component { };
 
-enum CloudLightListenMethod { CloudLight_MULTICAST, CloudLight_UNICAST };
+inline static int16_t sin16_c(uint16_t theta) {
+  static const uint16_t BASE[] = {0, 6393, 12539, 18204, 23170, 27245, 30273, 32137};
+  static const uint8_t SLOPE[] = {49, 48, 44, 38, 31, 23, 14, 4};
+  uint16_t offset = (theta & 0x3FFF) >> 3;  // 0..2047
+  if (theta & 0x4000)
+    offset = 2047 - offset;
+  uint8_t section = offset / 256;  // 0..7
+  uint16_t b = BASE[section];
+  uint8_t m = SLOPE[section];
+  uint8_t secoffset8 = uint8_t(offset) / 2;
+  uint16_t mx = m * secoffset8;
+  int16_t y = mx + b;
+  if (theta & 0x8000)
+    return -y;
+  return y;
+}
+inline static uint8_t half_sin8(uint8_t v) { return sin16_c(uint16_t(v) * 128u) >> 8; }
 
-class CloudLightComponent : public esphome::Component {
+class YellowAddressableLightEffect : public light::AddressableLightEffect {
  public:
-  CloudLightComponent();
-  ~CloudLightComponent();
+  explicit YellowAddressableLightEffect(const std::string &name) : light::AddressableLightEffect(name) {}
+  void apply(light::AddressableLight &addressable, const Color &current_color) override {
+    const Color yellow_color(150, 150, 0);
 
-  static const uint16_t PixelCount = 60;  // Set to the number of LEDs in your strip
-  static const uint8_t PixelPin = 27;     // Set to the pin you are using
-  NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip;
-
-  void setup() override;
-  void loop() override;
-  float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
-
-  void add_effect(YellowAddressableLightEffect *light_effect);
-  void add_effect(CyanAddressableLightEffect *light_effect);
-  void remove_effect(YellowAddressableLightEffect *light_effect);
-  void remove_effect(CyanAddressableLightEffect *light_effect);
+    const uint32_t now = millis();
+    uint8_t pos_add = 0;
+    if (now - this->last_progress_ > this->progress_interval_) {
+      const uint32_t pos_add32 = (now - this->last_progress_) / this->progress_interval_;
+      pos_add = pos_add32;
+      this->last_progress_ += pos_add32 * this->progress_interval_;
+    }
+    for (auto view : addressable) {
+      if (view.get_effect_data() != 0) {
+        const uint8_t sine = half_sin8(view.get_effect_data());
+        view = yellow_color * sine;
+        const uint8_t new_pos = view.get_effect_data() + pos_add;
+        if (new_pos < view.get_effect_data())
+          view.set_effect_data(0);
+        else
+          view.set_effect_data(new_pos);
+      } else {
+        view = Color::BLACK;
+      }
+    }
+    while (random_float() < this->twinkle_probability_) {
+      const size_t pos = random_uint32() % addressable.size();
+      if (addressable[pos].get_effect_data() != 0)
+        continue;
+      addressable[pos].set_effect_data(1);
+    }
+    addressable.schedule_show();
+  }
+  void set_twinkle_probability(float twinkle_probability) { this->twinkle_probability_ = twinkle_probability; }
+  void set_progress_interval(uint32_t progress_interval) { this->progress_interval_ = progress_interval; }
 
  protected:
-  // CloudLightListenMethod listen_method_{CloudLight_MULTICAST};
-  std::unique_ptr<socket::Socket> socket_;
-  std::set<YellowAddressableLightEffect *> yellow_light_effects_;
-  std::set<CyanAddressableLightEffect *> cyan_light_effects_;
+  float twinkle_probability_{0.05f};
+  uint32_t progress_interval_{4};
+  uint32_t last_progress_{0};
+};
+
+class CyanAddressableLightEffect : public light::AddressableLightEffect {
+ public:
+  explicit CyanAddressableLightEffect(const std::string &name) : light::AddressableLightEffect(name) {}
+  void apply(light::AddressableLight &addressable, const Color &current_color) override {
+    const Color cyan_color(0, 150, 150);
+
+    const uint32_t now = millis();
+    uint8_t pos_add = 0;
+    if (now - this->last_progress_ > this->progress_interval_) {
+      const uint32_t pos_add32 = (now - this->last_progress_) / this->progress_interval_;
+      pos_add = pos_add32;
+      this->last_progress_ += pos_add32 * this->progress_interval_;
+    }
+    for (auto view : addressable) {
+      if (view.get_effect_data() != 0) {
+        const uint8_t sine = half_sin8(view.get_effect_data());
+        view = cyan_color * sine;
+        const uint8_t new_pos = view.get_effect_data() + pos_add;
+        if (new_pos < view.get_effect_data())
+          view.set_effect_data(0);
+        else
+          view.set_effect_data(new_pos);
+      } else {
+        view = Color::BLACK;
+      }
+    }
+    while (random_float() < this->twinkle_probability_) {
+      const size_t pos = random_uint32() % addressable.size();
+      if (addressable[pos].get_effect_data() != 0)
+        continue;
+      addressable[pos].set_effect_data(1);
+    }
+    addressable.schedule_show();
+  }
+  void set_twinkle_probability(float twinkle_probability) { this->twinkle_probability_ = twinkle_probability; }
+  void set_progress_interval(uint32_t progress_interval) { this->progress_interval_ = progress_interval; }
+
+ protected:
+  float twinkle_probability_{0.05f};
+  uint32_t progress_interval_{4};
+  uint32_t last_progress_{0};
 };
 
 }  // namespace cloud_light
